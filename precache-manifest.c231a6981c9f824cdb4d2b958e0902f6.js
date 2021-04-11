07ee8fb90bfa1cf63be1da95ab2ed49e154a61c7
rajeshbanoth <banothrajesh97@gmail.com>
2021-04-11 20:05:32 +0530

    Updates

---
+++ precache-manifest.c231a6981c9f824cdb4d2b958e0902f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75166def6d0bd9c9875e1d846c2d046",
+    "url": "/openBook/index.html"
+  },
+  {
+    "revision": "bb72ce5642a6f911a9f2",
+    "url": "/openBook/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5e7017779341d00e39e7",
+    "url": "/openBook/static/css/main.db430fef.chunk.css"
+  },
+  {
+    "revision": "bb72ce5642a6f911a9f2",
+    "url": "/openBook/static/js/2.894b96ab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/openBook/static/js/2.894b96ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7017779341d00e39e7",
+    "url": "/openBook/static/js/main.96457fed.chunk.js"
+  },
+  {
+    "revision": "f9834dc7d64594bdd313",
+    "url": "/openBook/static/js/runtime-main.0eebe7aa.js"
+  }
+]);
